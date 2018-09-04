ffbdd783081c87101321881d524531422b4fecc3
lostfields <nicolai.gjaerum@gmail.com>
2018-09-04 22:08:24 +0200

    really, these shouldn't be here.

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router as CoreRouter, Request, Response } from 'express';
-import { Router } from './express/router';
-export default Router;
-export { Router, CoreRouter, Request, Response };
